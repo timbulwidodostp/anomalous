aa2291942068e942438acffbf3b4305eae6b886a
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-09-16 14:27:08 +0700

    Use anomalous With (In) R Software

---
+++ anomalous/anomalous.R
@@ -0,0 +1,18 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Anomalous time series Use anomalous With (In) R Software
+install.packages("remotes")
+remotes::install_github("robjhyndman/anomalous")
+library("anomalous")
+anomalous = read.csv("https://raw.githubusercontent.com/timbulwidodostp/anomalous/main/anomalous/anomalous.csv",sep = ";")
+# Estimation Anomalous time series Use anomalous With (In) R Software
+par(mfrow=c(1,2))
+anomalous <- tsmeasures(anomalous)
+biplot.features(anomalous)
+anomaly(anomalous)
+# Anomalous time series Use anomalous With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
